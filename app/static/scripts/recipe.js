9705429a8c939b0dc290f4b92ae972f8daa79d5f
Duncan Robertson <bacsdrobertson@gmail.com>
2017-08-23 17:07:43 -0400

    Minor app update

--- app/static/scripts/recipe.js
+++ app/static/scripts/recipe.js
@@ -1,27 +1,4 @@
 (function() {
 	'use strict';
-	angular.module('recipe', ['ngRoute'])
-	.config(['$routeProvider', '$locationProvider',
-		function($routeProvider, $locationProvider) {
-			$routeProvider
-			.when('/', {
-				templateUrl: 'views/main.html',
-				controller: 'MainController',
-				controllerAs: 'mainCtrl'
-			})
-			.when('/recipe/:recipeId', {
-				templateUrl: 'views/recipe.html',
-				controller: 'RecipeController',
-				controllerAs: 'recipeCtrl'
-			})
-			.when('/about', {
-				templateUrl: 'views/About.html'
-			})
-			.otherwise({
-				redirectTo: '/'
-			});
-
-			$locationProvider.html5Mode(true);
-		}
-	]);
+	angular.module('recipe', ['ngRoute']);
 })();
